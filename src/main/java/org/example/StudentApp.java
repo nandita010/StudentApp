b3bf353ebe26f6b1e8190fe53bbfc5f43c007621
NanditaReji-STUDENT <G00463584@atu.ie>
2025-10-08 17:29:45 +0100

    Added sc.nextLine();

--- src/main/java/org/example/StudentApp.java
+++ src/main/java/org/example/StudentApp.java
@@ -6,22 +6,32 @@ public class StudentApp {
 
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the number of students: ");
-          count = sc.nextInt();
+          int count = sc.nextInt();
+          int i = 0;
+         ArrayList<Student> students = new ArrayList<>();
 
-        while(count <= 3) {
+        while(count < 3) {
+
+            sc.nextLine();
+            System.out.println("Please enter your name: ");
+            String name = sc.nextLine();
+
+            System.out.println("Please enter your email: ");
+            String email = sc.nextLine();
+
+            System.out.println("Please enter your Student ID: ");
+            String studentID = sc.nextLine();
+
+            Student student1 = new Student();
+
+            i++;
+
+            student1.setName(name);
+            student1.setEmail(email);
+            student1.setStudentID(studentID);
         }
-
-        System.out.println("Please enter your name: ");
-        String name= sc.nextLine();
-
-        System.out.println("Please enter your email: ");
-        String email= sc.nextLine();
-
-        System.out.println("Please enter your Student ID: ");
-        String studentID= sc.nextLine();
-
 
 
     }
