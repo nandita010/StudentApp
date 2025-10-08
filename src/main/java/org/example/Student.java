94af4d532ea674b00b8710c063180759985e63e2
NanditaReji-STUDENT <G00463584@atu.ie>
2025-10-08 17:55:53 +0100

    Added getters and setters

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,7 +1,33 @@
 package org.example;
 
 public class Student {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String Email) {
+        this.name = name;
+    }
+    private String Email;
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setName(String Email) {
+        this.Email = Email;
+    }
+    private String StudentID ;
+
+    public String getstudentID() {
+        return StudentID;
+    }
+
+    public void setStudentID(String name) {
+        this.name = name;
+    }
 
 
 }
